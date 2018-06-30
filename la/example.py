d97f4f012b0405fc2922363e047b2f1d319d3044
Yungon Park <hahafree12@gmail.com>
2018-06-30 13:06:11 +0900

    MongoDB에 학습 데이터를 저장하기 위한 Endpoint 생성

--- la/example.py
+++ la/example.py
@@ -1,8 +1,31 @@
-from flask import Flask, render_template
+import json
+
+from pymongo import MongoClient
+from bson import ObjectId
+from flask import Flask, render_template, request, abort
 
 app = Flask(__name__)
 
 
+# Database configuration
+lrs = MongoClient('localhost')['LRS']['CaliperEvents']
+
+
 @app.route('/')
-def route():
+def index():
     return render_template('index.html')
+
+
+@app.route('/endpoint', methods=['POST'])
+def endpoint():
+    """ Endpoint to save learning record. """
+    if request.method == 'POST':
+        data = json.loads(request.data)
+        result = lrs.insert(data)
+
+        if type(result) is not ObjectId:
+            abort(500)
+
+        return str(result)
+    else:
+        abort(400)
