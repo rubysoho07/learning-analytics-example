07d187692f3bf426cc81645f4d961e4300faaa7f
Yungon Park <hahafree12@gmail.com>
2018-07-08 17:38:19 +0900

    표현 방식 변경

--- la/caliper_entity.py
+++ la/caliper_entity.py
@@ -23,16 +23,17 @@ def get_software_application():
 
 
 def get_group():
-    return entities.Group(id='http://example.org/group/test', name='testgroup')
+    return entities.Group(id='http://example.org/group/test', name='1학년 3반')
 
 
 def get_web_page():
-    return entities.WebPage(id='http://example.org/textbook/1', name='Textbook')
+    return entities.WebPage(id='http://example.org/textbook/1', name='불법 행위와 범죄(교과서)')
 
 
 def get_tags(user, tags):
     return entities.TagAnnotation(
         id="http://example.org/textbook/1/tag/1",
+        name="불법 행위와 범죄(교과서) 태그",
         annotator=get_user(user),
         annotated=get_web_page(),
         tags=tags
@@ -42,6 +43,7 @@ def get_tags(user, tags):
 def get_attempt(user):
     return entities.Attempt(
         id="http://example.org/assessment/1/attempt/1",
+        name="불법 행위와 범죄 평가(첫 번째 시도)",
         assignee=get_user(user),
         assignable=get_assessment(),
         count=1
@@ -52,6 +54,7 @@ def get_score(score):
     return entities.Score(
         id="http://example.org/assessment/1/attempt/1/score",
         attempt="http://example.org/assessment/1/attempt/1",
+        name="불법 행위와 범죄 평가(첫 번째 시도)",
         maxScore=10.0,
         scoreGiven=score,
         dateCreated=datetime.datetime.utcnow().isoformat(timespec='milliseconds') + 'Z'
@@ -59,4 +62,5 @@ def get_score(score):
 
 
 def get_assessment():
-    return entities.Assessment(id='http://example.org/assessment/1', name='불법 행위와 범죄 평가')
+    return entities.Assessment(id='http://example.org/assessment/1', name='불법 행위와 범죄'
+                                                                          '(평가)')
